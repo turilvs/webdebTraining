0357748173786a406c91f2b0c0302218eeae3a03
Rantala Artturi Kalle Nestori <artturi.rantala@hotmail.com>
2023-04-14 20:58:28 +0300

    paranneltu Comment Boxin toiminnallisuuksia

--- scripts.js
+++ scripts.js
@@ -13,28 +13,32 @@ const nickname = document.getElementById('nickname')
 const sendButton = document.getElementById('send-btn') 
 const cancelButton = document.getElementById('cancel-btn') 
 const comment = document.getElementById('comment')
-const output = document.getElementById('comment-view') 
+const output = document.getElementById('comments') 
 
 
 nickname.addEventListener('input', () => {
     let nick = document.createTextNode(nickname.value)
     console.log(nickname.innerText)
 })
-comment.addEventListener('input', () => {
-    let content = document.createTextNode(comment.value)
-    console.log(comment.value)
+comment.addEventListener('keyup', function(event) {
+    
+    if (event.key === "Enter") {
+        event.preventDefault()
+        sendMessage(comment.value)
+    }
 })
 
-function sendMessage(content) {
+function sendMessage() {
 
+    let content = `${nickname.value}: ${comment.value}`
     const p = document.createElement('p');
         p.className = 'message'
         // p.id = 
         p.textContent = content
         output.appendChild(p)
+        comment.value = ''
 }
 
 sendButton.addEventListener('click', () => {    
-    let s = `${nickname.value}: ${comment.value}`
-    sendMessage(s)
+    sendMessage()
 })
